967bea44a02e11c2234f306c0cc1baaedc38b642
AndersonKeller <andersonkellervendas@hotmail.com>
2026-05-03 22:51:21 -0300

    removido a chamada pra API

--- app/app.vue
+++ app/app.vue
@@ -1,27 +1,27 @@
 <script setup lang="ts">
 import "./assets/global.css";
-import { uuseConfigsStore } from "./stores/configs.store";
+
 // import favicon
 
-const { data } = await useFetch("/api/configs");
+// const { data } = await useFetch("/api/configs");
 const route = useRoute();
 
-if (data.value.styles) {
-  if (import.meta.client) {
-    const style = document.createElement("style");
-    // style.innerHTML = data.value.styles;
-    let content = "";
-    for (const key in data.value.styles) {
-      content += `${key}:${data.value.styles[key]};`;
-    }
+// if (data.value.styles) {
+//   if (import.meta.client) {
+//     const style = document.createElement("style");
+//     // style.innerHTML = data.value.styles;
+//     let content = "";
+//     for (const key in data.value.styles) {
+//       content += `${key}:${data.value.styles[key]};`;
+//     }
 
-    style.innerHTML = `:root{${content}}`;
-    document.head.append(style);
-  }
-}
-uuseConfigsStore().setConfigs(data.value);
+//     style.innerHTML = `:root{${content}}`;
+//     document.head.append(style);
+//   }
+// }
+// uuseConfigsStore().setConfigs(data.value);
 useHead(() => ({
-  title: `${data.value.titulo} | ${route.meta.title}`,
+  title: `Subto Hellemento | ${route.meta.title}`,
   // link: [{ rel: "icon", type: "image/svg+xml", href: data.value.favicon }],
 }));
 </script>
